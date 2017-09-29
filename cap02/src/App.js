829c121a254622301f76bf56708d2f6407d9a459
pxai <p@pello.io>
2017-09-29 19:34:10 -0400

    Added second sample

--- cap02/src/App.js
+++ cap02/src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header'
+import Paragraph from './Paragraph'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header logo={logo} />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Paragraph />
       </div>
     );
   }
--- cap02/src/Header.js
+++ cap02/src/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
         <header className="App-header">
           <img src={this.props.logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>>
+        </header>
     );
   }
 }
