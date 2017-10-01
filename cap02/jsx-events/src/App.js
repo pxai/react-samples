60af3b129724126a87f5c1117a879b3b32aa3a8a
pxai <p@pello.io>
2017-10-01 13:42:27 +0200

    Ok, added CSS sample

--- cap02/jsx-events/src/App.js
+++ cap02/jsx-events/src/App.js
@@ -2,50 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  arrowHandleClick = (e) => {
+    e.preventDefault();
+    alert('The first link was clicked.');
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    alert('The second link was clicked.');
+  }
+
   render() {
-    const friends = [
-      { name: "John Doe", age: 34}, 
-      { name: "Gandalf", age: 1432},
-      { name: "Aragorn", age: 442}
-    ];
-
-    const friendElements = friends.map(function (e) {
-        return (
-          <li key={e.name} ><b>{e.name}</b>: {e.age} years</li>
-        )
-    });
-
-    let showTitle = false;
-
-    let applyCss = true;
 
     return (
       <div >
-        <h1>My Friends {/* Comments inside elements with braces! */}</h1>
-        <ul 
-          /* This
-            is
-            a multiline
-          */
-        >
-          {friendElements}
-        </ul>
-        
-        {showTitle?
-         (
-            <h2>We show title</h2>
-         ):(
-            <p>No title</p>
-         )
-        }
 
-         {console.log('This will appear in the console')}
-        <p className={ (applyCss) ? 'App-intro':''} >
-          This is a paragraph
-        </p>
-        <p onClick={() => alert('It works')}>
-          Click on this paragraph
-          </p>
+      <button onClick={() => alert('It works')}>
+          Click on this button
+      </button><br />
+
+      <a href="#" onClick={this.arrowHandleClick}>
+        Click first link
+      </a><br />
+
+      <a href="#" onClick={(e) => this.handleClick(e)}>
+        Click second link
+      </a><br />
       </div>
     );
   }
---
+++ cap02/react-css/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Styled component</h1>
+        </header>
+        <main>
+          <h2 id="supertitle">Main content</h2>
+          <p className="regular">This component is very nice</p>
+          </main>
+      </div>
+    );
+  }
+}
+export default App;
