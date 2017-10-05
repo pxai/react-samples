21b6178c73854355f2e4f4cf6b4cc23adaff05b8
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-10-05 20:35:53 +0200

    Ok, added state sample

--- cap02/react-state/src/App.js
+++ cap02/react-state/src/App.js
@@ -3,21 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  static defaultState =  {
-    number: 0,
+
+  constructor (props) {
+    super(props);
+    this.state = {
+     title: 'Default',
+     time: new Date().toLocaleTimeString(),
+     number: 0,
+     numbers: [] 
+    }
+  }
+
+  changeState () {
+    let number = Math.round(Math.random() * 4);
+    let numbers = this.state.numbers;
+    numbers.push(number);
+    this.setState({
+        time: new Date().toLocaleTimeString(),
+        numbers: numbers,
+        number: number,
+        title: ((number % 2 === 0)?'It is even':'It is odd')
+      })
+    console.log('changeState> ', this.state);
   }
 
   render() {
+    console.log('Render was called> ', this.state);
+    const colors = ['red', 'yellow', 'green', 'blue','orange'];
+    const color = colors[this.state.number];
+
     return (
-      <div className="App">
+      <div className="App" style={{backgroundColor: color}}>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.props.title}</h1>
+          <h1 className="App-title">
+            {this.state.title} - {this.state.number}
+          </h1>
         </header>
-        <p className="App-intro">
-          {this.state.number}
-          <button></button>
-        </p>
+        <div className="App-intro">
+          <div>{this.state.time}</div>
+          Press this button to change state
+          <div><button onClick={() => this.changeState()}>
+            Change state
+           </button>
+           </div>
+           <div>
+             Generated numbers
+             <ul>
+               {this.state.numbers.map((x) => (
+                 <li key={x}>{x}</li>
+               ))}
+               </ul>
+             </div>
+        </div>
       </div>
     );
   }
