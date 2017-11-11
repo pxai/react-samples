058fdeac519837ff58cc1f1635ab90b611fc976e
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-11-11 09:00:46 +0100

    Added complex form sample

---
+++ cap02/react-complex-form/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { login: '', password: '' }
+  }
+
+  handleSubmit=  (e) => {
+    e.preventDefault();
+    this.setState({
+      login: this.refs.login.value,
+      password: this.refs.password.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Login form</h1>
+        </header>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+          <div><label>Login</label></div>
+          <div>
+            <input type="text" ref="login"/>
+          </div>
+          <div><label>Password</label></div>
+          <div>
+            <input type="password" ref="password"/>
+          </div>
+          <div>
+            <input type="submit" value="Login" />
+          </div>
+          </form>
+          </div>
+          <div>Login: {this.state.login}, password: {this.state.password}
+            </div>
+      </div>
+    );
+  }
+}
+
+export default App;
