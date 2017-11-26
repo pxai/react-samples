f0fb6ba0fd55d6c0774bb4ac022425d18e743bde
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-11-26 20:54:10 +0100

    Ok, first working version of react router

---
+++ cap02/react-router/src/About.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './App.css';
+
+
+
+class About extends Component {
+
+  render() {
+
+    return (
+      <div className="app">
+        <div className="app-header">
+            <div className="lapp-header-title">
+              <h1>About component</h1>
+            </div>
+            <div>
+              <Link to="/">Home</Link> | 
+              <Link to="/about">About</Link> | 
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default About;
---
+++ cap02/react-router/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+import Default  from './Default';
+import NotFound  from './NotFound';
+import Sample  from './Sample';
+import About  from './About';
+import Category  from './Category';
+import './App.css';
+
+
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="app">
+        <div className="app-header">
+            <div className="lapp-header-title">
+              <h1>Routing sample</h1>
+            </div>
+            <div>
+              <Link to="/">Home</Link> | 
+              <Link to="/about">About</Link> | 
+            </div>
+            <Switch>
+              <Route exact path='/' component={Default}/>  
+              <Route exact path='/404' component={NotFound}/> 
+              <Route path='/sample/:id/:name' component={Sample} />
+              <Route exact path='/about' component={About} />
+              <Route path='/:category' component={Category} />
+              <Route component={NotFound} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
