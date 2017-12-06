e4998dcdcc422f964775b89b43696658013aa42f
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-12-06 19:57:56 +0100

    fixed basic project

--- cap03/react-newspaper/src/App.js
+++ cap03/react-newspaper/src/App.js
@@ -16,18 +16,18 @@ class App extends Component {
       <div className="app">
         <div className="list-books">
             <div className="list-books-title">
-              <h1>Pello's MyReads</h1>
+              <h1>React News</h1>
             </div>
             <div>
-              <Link to="/">Home</Link> | 
-              <Link to="/about">About</Link> | 
+              <Link to="/">Home</Link> |
+              <Link to="/about">About</Link> |
             </div>
             <Switch>
-              <Route exact path='/' component={PostList}/>  
-              <Route exact path='/404' component={NotFound}/> 
+              <Route exact path='/' component={PostList}/>
+              <Route exact path='/404' component={NotFound}/>
               <Route path='/post/:category/:id' component={PostDetail} />
               <Route exact path='/postList' component={PostList} />
-              <Route exact path='/deleted' component={PostList}/> 
+              <Route exact path='/deleted' component={PostList}/>
               <Route exact path='/about' component={About} />
               <Route path='/:category' component={PostList} />
               <Route component={NotFound} />
--- cap03/simple-redux/src/index.js
+++ cap03/simple-redux/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
