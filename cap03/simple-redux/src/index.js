d5f951c9366c1b557cb826d74ea93d3a63af6c0d
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-12-07 23:57:17 +0100

    Ok, done

--- cap03/simple-redux/src/index.js
+++ cap03/simple-redux/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {  BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+                    <Router>
+                        <App />
+                        </Router>
+                </Provider>, document.getElementById('root'));
 registerServiceWorker();
