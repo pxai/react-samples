520291ed32eb856ac49b273d1a33449f7fc889f7
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-12-07 23:59:36 +0100

    added store

--- cap03/simple-redux/src/index.js
+++ cap03/simple-redux/src/index.js
@@ -6,6 +6,25 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+const logger = store => next => action => {
+    console.group(action.type)
+    console.info('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd(action.type)
+    return result
+  }
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+  const store = createStore(
+    reducer,
+    composeEnhancers(
+      applyMiddleware(thunk, logger)
+    )
+  )
 
 ReactDOM.render(<Provider store={store}>
                     <Router>
