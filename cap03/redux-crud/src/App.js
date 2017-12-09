94f6e0b586a6313a51caa8ee6409e1a2ff08b45f
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-12-09 08:12:20 +0100

    done with simple, starting crud

--- cap03/redux-crud/src/App.js
+++ cap03/redux-crud/src/App.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux'
+import { showName, changeName } from './actions'
+import logo from './img/logo.svg';
+import reduxLogo from './img/redux.svg'
 import './App.css';
 
 class App extends Component {
+  change = () => {
+    console.log('Changed.');
+    this.props.appChangeName(this.refs.name.value);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={reduxLogo} className="App-logo" alt="redux logo" />
+          <h1 className="App-title">{this.props.appName}</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <input type="text" ref="name" />
+          <button onClick={() => this.change()}>
+          Change store
+          </button>
         </p>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps (state, props) {
+  return {
+    appName: state.name
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    appShowName: () => dispatch(showName()),
+    appChangeName: (data) => dispatch(changeName(data))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
--- cap03/redux-crud/src/index.js
+++ cap03/redux-crud/src/index.js
@@ -6,11 +6,9 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import reducer from './reducer'
+import reducer from './reducers'
 
-  const store = createStore(
-    reducer: reducer
-  )
+  const store = createStore( reducer )
 
 ReactDOM.render(<Provider store={store}>
                     <Router>
