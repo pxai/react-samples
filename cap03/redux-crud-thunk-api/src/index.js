7d349cceb2c0dedce2c7da5f99a2d5fe0bc4d6f0
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-12-11 23:58:17 +0100

    Git added thunk support

--- cap03/redux-crud-thunk-api/src/index.js
+++ cap03/redux-crud-thunk-api/src/index.js
@@ -7,8 +7,26 @@ import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import thunk from 'redux-thunk';
 
-  const store = createStore( reducer )
+const logger = store => next => action => {
+    console.group(action.type)
+    console.info('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd(action.type)
+    return result
+  }
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(thunk, logger)
+  )
+)
 
 ReactDOM.render(<Provider store={store}>
                     <Router>
