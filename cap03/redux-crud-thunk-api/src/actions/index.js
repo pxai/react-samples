9d8302b8f5d8d5997f859514968b473d335237b7
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-12-13 12:34:05 +0100

    Mixed api with actions

--- cap03/redux-crud-thunk-api/src/actions/index.js
+++ cap03/redux-crud-thunk-api/src/actions/index.js
@@ -6,24 +6,43 @@ export const UPDATE_NOTE = 'UPDATE_NOTE'
 export const DELETE_NOTE = 'DELETE_NOTE'
 
 
-export function getNotes () {
+export function getNotes (notes) {
     return {
-      type: GET_NOTES
+      type: GET_NOTES,
+      notes
     }
   }
 
-export function getNote ( id ) {
+  export function getNotesAsync (notes) {
+      return dispatch => (
+          getAll().then(notes => dispatch(getNotes(notes)))
+      )
+    }
+
+export function getNote ( note ) {
   return {
     type: GET_NOTE,
-    id
+    note
   }
 }
+
+export function getNoteAsync (id) {
+    return dispatch => (
+        getAll().then(note => dispatch(getNote(note)))
+    )
+  }
 
 export function addNote ( note ) {
   return {
     type: ADD_NOTE,
     note
   }
+}
+
+export function addNoteAsync(note) {
+    return dispatch => (
+        create(note).then(() => dispatch(addNote(note)))
+    )
 }
 
 export function updateNote ( note ) {
@@ -33,9 +52,21 @@ export function updateNote ( note ) {
   }
 }
 
+export function updateNoteAsync(note) {
+    return dispatch => (
+        update(note).then(() => dispatch(updateNote(note)))
+    )
+}
+
 export function deleteNote ( id ) {
   return {
     type: DELETE_NOTE,
     id
   }
+}
+
+export function deleteNoteAsync(id) {
+    return dispatch => (
+        delete(id).then(() => dispatch(deleteNote(id)))
+    )
 }
