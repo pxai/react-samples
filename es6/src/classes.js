51a2b6a3171e821945cc27392c538f92547c58e6
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-12-15 12:20:32 +0100

    adding basic samples

---
+++ es6/src/classes.js
@@ -0,0 +1,19 @@
+class Device {
+    constructor () {
+        console.log('Device was created')
+    }
+
+    doStuff () {
+        console.log('Doing some stuff')
+    }
+
+    //doThis = () => (console.log('I do some'))
+    //doOtherStuff = (a) => console.log('Just showin it: ', a);
+
+    //doMoreStuff = (a,b) => a+b
+    
+    /*doMultipleStuff = (a, b = 0) => {
+        console.log('Passed values: ', a, b)
+        a + b
+    }*/
+}
---
+++ es6/src/variables.js
@@ -0,0 +1,10 @@
+someVar = 6
+
+var otherVar = 42
+
+const pi = 3.1415
+// pi = 666  This would cause errors
+
+let number = 0
+number++
+let thereBeRock = 'ACDC'
