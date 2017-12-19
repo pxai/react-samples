9c941ae3fa0450d5e2fd98d6d3ea85c60c0039f2
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-12-19 23:33:29 +0100

    ok

--- cap02/react-i18n/src/index.js
+++ cap02/react-i18n/src/index.js
@@ -6,10 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
-import { I18n, I18nextProvider,  Trans } from 'react-i18next';
 import { reactI18nextModule } from 'react-i18next';
-
-
 
 i18n
   .use(LanguageDetector)
@@ -17,17 +14,9 @@ i18n
   .use(reactI18nextModule)
   .init({
     fallbackLng: 'en',
-
-    // have a common namespace used around the full app
     ns: ['translations'],
     defaultNS: 'translations',
-
     debug: true,
-
-    interpolation: {
-      escapeValue: false, // not needed for react!!
-    },
-
     react: {
       wait: true
     }
