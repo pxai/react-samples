30e02b647c0fc0f00df85eac4e23788b70706bca
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-12-22 23:51:54 +0100

    Added components

---
+++ cap02/react-material-ui/src/component/AppBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './App.css';
+
+class AppBar extends Component {
+
+  render() {
+
+    return (
+      <div className="app">
+        <div className="app-header">
+            <div className="lapp-header-title">
+              <h1>Default component</h1>
+            </div>
+            <div>
+              <Link to="/">Home</Link> | 
+              <Link to="/about">About</Link> |
+         </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AppBar;
