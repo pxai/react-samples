de967a5e873c39a7aa4ef59acaea3a57616e9ef1
pxai <pello_altadill@cuatrovientos.org>
2018-03-24 14:18:01 +0100

    Ok, go ahead

---
+++ js/vars.js
@@ -0,0 +1,39 @@
+var number = 42;
+number = 42;       // global!!
+
+var evil = 666
+var evil = 666; var good = 3
+
+var counter = 0;
+var age = -35;
+var weight = 5.673;
+var huge = 23e3;
+var chuckNorrisStrength = Infinity;
+var chuckNorrisFear = -Infinity;
+
+var name = 'Pello';
+var surname = "Altadill";
+var phrase = "These aren't the droids you're looking for";
+var greet = 'Hello world';
+var sheSaid = "We'll be \"friends\"";
+
+var reactRules = true;
+var iAmInmortal = false;
+
+var something = null;
+var unknown = undefined;
+
+var beast = 666;
+typeof beast;   //"number"
+delete beast;
+typeof beast;   // undefined
+
+var number = 'example';
+typeof number * 666:      // NaN
+
+
+var number;
+var counter = 0;
+var a, b=42, c = 4;
+var x,y,z;
+x = y = z = 0;
