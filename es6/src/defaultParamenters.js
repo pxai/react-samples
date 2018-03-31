d9a59f04d9f434e7930bcc2489e10bc1caf3fa04
pxai <pello_altadill@cuatrovientos.org>
2018-04-01 00:20:21 +0200

    Added quite a lot

--- es6/src/defaultParamenters.js
+++ es6/src/defaultParamenters.js
@@ -21,7 +21,7 @@ console.log(sample('John',5));    // JohnJohnJohnJohnJohn
 console.log(sample(undefined, 3));
 
 
-function paint ([r='ff', g='ff', b='ff']) {
+function paint ([r='ff', g='ff', b='ff']=[]) {
     return `color: #${r}${g}${b}`;
 }
 
@@ -31,5 +31,40 @@ console.log(paint([]));                    // color: #ffffff
 console.log(paint(['DE','AD']));           // color: #DEADff
 
 
+console.log(paint()); // error unless =[] is defined in function params
+
+function render ({ tag= 'div', cssClass='title', text='ok'}) {
+    return `<${tag} class="${cssClass}">
+                ${text}</${tag}>`;
+}
 
 
+console.log(render({}));
+// <div class="title">
+//    ok</div>
+
+console.log(render({tag: 'h1', cssClass: 'big', text: 'ES6 rlz'}));
+// <h1 class="big">
+//    ES6 rlz</h1>
+
+
+function validate ({ fields = [...{fields}], regex = [...regexp] }) {
+    for (f of fields) {
+        console.log(f.name);
+    }
+
+    for (r of regex)  {
+        console.log(r);
+    }
+}
+
+validate({fields: [{name:'login', value: 'falken'},
+                    {name: 'password', value: 'josua'}
+                  ],
+          regex : ['\\w{8,50}', '\\w\\W{8,15}']}
+          );
+
+// login
+// password
+// \w{8,50}
+// \w\W{8,15}
