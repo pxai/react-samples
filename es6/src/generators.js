d2d1dc393f5dcd6a7000703bce69d2bb1a15b4ef
pxai <pello_altadill@cuatrovientos.org>
2018-04-01 15:15:28 +0200

    Lots of new stuff

---
+++ es6/src/generators.js
@@ -0,0 +1,54 @@
+function* count(){
+    for (var i = 0; i < 3; i++) {
+        // This is stops the execution
+        yield i;
+    }
+}
+
+const iterator = count();
+
+console.log(iterator.next()); // { value: 0, done: false }
+console.log(iterator.next()); // { value: 1, done: false }
+console.log(iterator.next()); // { value: 2, done: false }
+console.log(iterator.next()); // { value: undefined, done: true }
+
+
+function* greet() {
+    let msg = yield;
+    console.log(`The message is ${msg}`);
+}
+
+const greetIterator = greet();
+greetIterator.next();
+greetIterator.next('Hello there');
+// The message is Hello there
+
+function* invoice (items){
+    let total = 0;
+    for (item of items) {
+        let cost = yield item.id;
+        console.log(cost, item);
+        total = total + cost * item.qty;
+    }
+    yield total;
+}
+
+const prices = { 1: 42.0, 5: 66.6}
+const purchase = [{id: 1, qty: 3},{ id: 5, qty: 2}];
+const invoiceIterator = invoice(purchase);
+
+console.log(invoiceIterator.next()); // Starts running.
+
+console.log(invoiceIterator.next(prices[1])); // { value: 1, done: false }
+console.log(invoiceIterator.next(prices[5])); // { value: 5, done: false }
+
+console.log(invoiceIterator.next()); // { value: 259.2, done: false }
+
+invoiceIterator.next(); // { value: undefined, done: true }
+
+
+
+
+/*for (var x of count()) {
+    console.log(x);
+}*/
---
+++ es6/src/promises.js
@@ -0,0 +1,42 @@
+const hello = function (msg) {
+    // Return a new promise.
+    return new Promise(function(resolve, reject) {
+        setTimeout(function () {
+            if (msg.length > 0) {
+                resolve(msg);
+            } else {
+                reject(Error('Msg not valid'));
+            }
+        }, 1000);
+    });
+
+}
+
+
+
+
+
+hello('Epa').then(function (msg) {
+    console.log('Ok, mesg is: ' + msg);
+}, function (error) {
+    console.log(error);
+});
+// Ok, mesg is: Epa
+
+/*
+hello('').then(function (msg) {
+    console.log('Ok, mesg is: ' + msg);
+}, function (error) {
+    console.log(error);
+});*/
+
+// Error: Msg not valid
+// + ... errors
+
+hello('').then(function (msg) {
+    console.log('Ok, mesg is: ' + msg);
+}).catch((err) => {
+    console.log(`Errors under control. ${err}`)
+});
+
+// Errors under control. Error: Msg not valid
