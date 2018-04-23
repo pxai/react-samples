309eafec1cf3011a60c16736b66c500ef707485e
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-04-23 23:59:48 +0200

    Added getter setter sample

---
+++ es6/src/getSet.js
@@ -0,0 +1,30 @@
+class Customer {
+    constructor (name, age) {
+        this._name = name;
+        this._age = age;
+    }
+
+   get name () {
+   	return this._name;
+   }
+
+   set name (name) {
+   	this._name= name;
+   }
+
+   get age () {
+   	return this._age;
+   }
+
+   set age (age) {
+   	this._age = age;
+   }
+}
+
+customer = new Customer();
+
+customer.age = 66;
+customer.name = 'John Doe';
+
+console.log(customer);
+console.log(customer.name, customer.age);
