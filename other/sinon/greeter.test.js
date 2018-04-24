a41779eace3d5534eea353909f2cc2801b7f251a
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-04-24 23:51:48 +0200

    ok, working

--- other/sinon/greeter.test.js
+++ other/sinon/greeter.test.js
@@ -5,4 +5,14 @@ describe('Tests for greeter', () => {
 	it('Should exist as a class', () => {
 		assert(Greeter);
 	});
+
+	it('Should return default message', () => {
+		const greeter = new Greeter();
+		assert.equal(greeter.msg,'Hello World');
+	});
+	
+	it('Should return default message', () => {
+		const greeter = new Greeter('Hi there');
+		assert.equal(greeter.msg,'Hi there');
+	});
 })
