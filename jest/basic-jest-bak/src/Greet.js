b11b885c2a6d5a8409f714edff9d7878a2f953d7
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-05-08 23:16:46 +0200

    Added tests

---
+++ jest/basic-jest-bak/src/Greet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Greet.css';
+
+class Greet extends Component {
+  render() {
+    return (
+      <div className="Greet">
+		This is a greet
+      </div>
+    );
+  }
+}
+
+export default Greet;
