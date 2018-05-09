efceaf3a2bc3d53ed67b1ef02bd78ac9317b6e0c
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-05-09 12:11:25 +0200

    Now jest testing work

--- jest/basic-jest/src/Greet.test.js
+++ jest/basic-jest/src/Greet.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import Greet from './Greet';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
+/*it('renders Greet without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Greet />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
+*/
+test('it matches snapshot', () => {
+	const component = renderer.create(<Greet />);
+ let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
