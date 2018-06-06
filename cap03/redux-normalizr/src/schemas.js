c3cdb1fe998ad500aa12a0d9472640cc2da7a2da
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-06-06 23:57:40 +0200

    Added normalizr schema

---
+++ cap03/redux-normalizr/src/schemas.js
@@ -0,0 +1,17 @@
+import { Schema, arrayOf } from 'normalizr';
+
+const projectSchema = Schema('projects');
+const membersSchema = Schema('members');
+const employeeSchema = Schema('empleoyee');
+
+// A project has an array of members:
+projectSchema.define({
+	members: arrayOf(membersSchema)
+});
+
+membersSchema.define({
+	employee: employeeSchema
+});
+
+
+export { projectSchema, membersSchema, productSchema };
