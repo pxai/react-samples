bf0252a95ff3384d0b2699747ed70f6c8f895424
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-06-26 19:34:40 +0200

    ok

--- cap04/react-native-components/App.js
+++ cap04/react-native-components/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+	  	<View>
+			<Text style={styles.subtitle}>This is another Text</Text>
+			<Text style={styles.paragraph}>This is a subtext</Text>
+		</View>
+	  	<View>
+			<MaterialIcons
+				name='directions-run'
+				color={'black'}
+				size={42}
+			/>
+		</View>
       </View>
     );
   }
@@ -19,5 +29,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  subtitle: {
+	fontSize: '2em',	  
+  },
+  paragraph: {
+	fontSize: '0.9em',
+	fontVariant: 'italic',
   },
 });
