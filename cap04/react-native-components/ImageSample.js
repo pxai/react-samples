dabfcfe6f4f81a941dff13d14244e49c604efbd2
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-06-30 11:48:34 +0200

    Latest changes

---
+++ cap04/react-native-components/ImageSample.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default class ScrollViewSample extends React.Component {
+  render() {
+    return (
+      <ScrollView style={styles.scrollview}>
+        <Text>This is a ScrollView</Text>
+	  	<View>
+			<Text>Scroll views are nice</Text>
+		</View>
+	  	<View>
+            <Text>This component is awesome</Text>
+		</View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 10
+  },
+});
