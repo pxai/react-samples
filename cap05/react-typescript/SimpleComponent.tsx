88ee51657884499e1baf09327717a0bc44e02f3b
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-08-30 22:34:09 +0200

    Added component

--- cap05/react-typescript/SimpleComponent.tsx
+++ cap05/react-typescript/SimpleComponent.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 
 class SimpleComponent extends React.Component <{}> {
+
 	render () {
 		return (
 			<div>
 				<h2>This component was created with TypeScript</h2>
 			</div>
-		);	
-	}	
+		);
+	}
 }
 
 export default SimpleComponent;
