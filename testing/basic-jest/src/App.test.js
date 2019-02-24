d9d0fd4ccf627afd71731db32a29e08358adfb75
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-02-25 00:10:04 +0100

    Adde eslint

--- testing/basic-jest/src/App.test.js
+++ testing/basic-jest/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+test('it matches snapshot', () => {
+	const component = renderer.create(<App />);
+ let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
