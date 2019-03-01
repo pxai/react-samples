b349de910a9a8f33f262c76a5a610f9d79810b66
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-03-01 09:27:11 +0100

    Added tests

--- testing/basic-jest/src/Api.js
+++ testing/basic-jest/src/Api.js
@@ -1,5 +1,11 @@
 class Api {
+    constructor (data = {}) {
+        this._data = data;
+    }
 
+    list () {
+        return this._data;
+    }
 }
 
 export default Api;
--- testing/basic-jest/src/Api.test.js
+++ testing/basic-jest/src/Api.test.js
@@ -1,5 +1,17 @@
 import Api from './Api';
 
-test('renders without crashing', () => {
+test('Api class instantiates', () => {
     const api = new Api(); 
+});
+
+test('Default data is empty', () => {
+    const api = new Api();
+
+    expect(api.list()).to.equal({});
+});
+
+test('Data passed to constructor returrns with list', () => {
+    const api = new Api({id: 1, task: "Work"});
+
+    expect(api.list()).to.equal({id: 1, task: "Work"});
 });
