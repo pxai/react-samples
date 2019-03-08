75d4dd6523f246b3bb762fe29dfb06a710b579de
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-03-08 23:27:27 +0100

    Added test

--- testing/basic-jest/src/Form.js
+++ testing/basic-jest/src/Form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+gmport React, { Component } from 'react';
 
 class Form extends Component {
     constructor () {
--- testing/basic-jest/src/Form.test.js
+++ testing/basic-jest/src/Form.test.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Form from './Form';
 import renderer from 'react-test-renderer';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Form />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 
 test('it matches snapshot', () => {
-	const component = renderer.create(<App />);
+	const component = renderer.create(<Form />);
  let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
