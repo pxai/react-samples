9ae849a96707e52455ae77d907dfb3b1b09e33ab
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-03-13 09:33:28 +0100

    Corrected task stuff

--- testing/basic-jest/src/TaskList.test.js
+++ testing/basic-jest/src/TaskList.test.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom';
 import TaskList from './TaskList';
 import renderer from 'react-test-renderer';
 
+const task = { id: 2, task: "Do it"};
+
 test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<TaskList />, div);
+  ReactDOM.render(<TaskList {...task } />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 
 test('it matches snapshot', () => {
-	const component = renderer.create(<TaskList />);
+ const component = renderer.create(<TaskList {...task} />);
  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+ expect(tree).toMatchSnapshot();
 });
 
