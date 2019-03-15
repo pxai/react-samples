1025644530647ca0cb2d4c53ceb5c8c8c84fa5ea
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-03-15 09:58:54 +0100

    Added links

--- testing/basic-jest/src/Task.js
+++ testing/basic-jest/src/Task.js
@@ -3,8 +3,18 @@ import React, { Component } from "react";
 class Task extends Component {
     render () {
         return <div>
-                <div>{this.props.task.id}</div>
-                <div>{this.props.task.task}</div>
+                <div>{this.props.task.id}
+                - {this.props.task.task}</div>
+               <div>
+                <a href="javascript:void(0)"
+                   onClick={() => this.props.api.delete(this.props.task.id)}>
+                   Update
+                </a>
+                <a href="javascript:void(0)"
+                   onClick={() => this.props.api.delete(this.props.task.id)}>
+                    Delete
+                </a>
+               </div>
                </div>;
     }
 }
