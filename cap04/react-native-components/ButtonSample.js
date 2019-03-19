1986871d9f211233e6233903f45022dbacf04dec
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-03-19 22:49:04 +0100

    Added picker

---
+++ cap04/react-native-components/ButtonSample.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { AppRegistry, TextInput } from 'react-native';
+
+export default class PickerSample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { framework: 'React' };
+  }
+
+  selectionChanged (value, index) {
+    this.setState({framework: value});
+    console.log("Button was pressed: ", this.state.framwork);
+  }
+
+  render() {
+    return (
+      <Picker
+        selectedValue={this.state.framework}
+        style={style.picker}
+        onValueChange={this.selectionChanged.bind(this, value, index)}>
+        <Picker.Item label="React" value="react" />
+        <Picker.Item label="Vue" value="vue" />
+        <Picker.Item label="Angular" value="angular" />
+      </Picker>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    picker: {
+      height: 50, width: 100
+    },
+});
