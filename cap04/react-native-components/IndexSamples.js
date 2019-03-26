9f364348d9e02d7a642389b82cd40bedc394a3a3
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-03-26 08:33:16 +0100

    Almost there

--- cap04/react-native-components/IndexSamples.js
+++ cap04/react-native-components/IndexSamples.js
@@ -1,3 +1,13 @@
 import { ButtonSample } from "./ButtonSample";
+import { PickerSample } from "./PickerSample";
+import { TextViewSample } from "./TextViewSample";
+import { TextInputSample } from "./TextInputSample";
+import { SwitchSample } from "./SwitchSample";
 
-export ButtonSample;
+export default { 
+    ButtonSample,
+    PickerSample,
+    TextViewSample,
+    TextInputSample,
+    SwitchSample
+};
