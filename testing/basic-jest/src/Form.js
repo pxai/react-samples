fc2977805179ca310287770e27d6e650d964b33f
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-03-30 23:12:13 +0100

    Corrected again

--- testing/basic-jest/src/Form.js
+++ testing/basic-jest/src/Form.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+    constructor () {
+        super();
+        this._task = this.props.task.task;
+    }
     render () {
         return <div>
                 <div><label>Task</label></div>
-                <div><input type="text" ref="task" /></div>
+                <div><input type="text" ref="task" value={this._task} /></div>
                 <div>
                     <input type="button" value="Save" onClick={this.save.bind(this)} />
                 </div>
