81da64a8c25d11182be0342ff2ffc23036b7a8a2
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-04-03 00:19:55 +0200

    ok good

--- testing/basic-jest/src/Form.js
+++ testing/basic-jest/src/Form.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-    constructor () {
-        super();
-        this._task = this.props.task.task;
+    constructor (props) {
+        super(props);
+        this.state = { task: props.task.task };
     }
+
     render () {
         return <div>
                 <div><label>Task</label></div>
-                <div><input type="text" ref="task" value={this._task} /></div>
+                <div><input type="text" onChange={this.updateTask.bind(this)} ref="task" value={this.state.task} /></div>
                 <div>
                     <input type="button" value="Save" onClick={this.save.bind(this)} />
                 </div>
                </div>;
+    }
+
+    updateTask () {
+        this.setState({ task: this.refs.task.value });
     }
 
     save () {
--- testing/basic-jest/src/Greet.js
+++ testing/basic-jest/src/Greet.js
@@ -3,19 +3,20 @@ import './Greet.css';
 
 class Greet extends Component {
   constructor(props) {
-	super(props);
-	this.state = {
-	message: 'This is a greet'
-	};
+    super(props);
+    this.state = {
+    message: 'This is a greet'
+    };
   }
   doSomething(){
-	console.log('Greet was clicked');
-	this.setState({message: 'I was clicked'});
+    console.log('Greet was clicked');
+    this.setState({message: 'I was clicked'});
   }
+
   render() {
     return (
       <div className="Greet" onClick={() => this.doSomething()}>
-		{this.state.message}
+		  {this.state.message}
       </div>
     );
   }
