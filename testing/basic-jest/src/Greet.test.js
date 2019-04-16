22badca3bc35e8ea4e9d3ffb3dbd3ecd1bd04377
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-04-16 09:55:07 +0200

    Added test for greet

--- testing/basic-jest/src/Greet.test.js
+++ testing/basic-jest/src/Greet.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Greet from './Greet';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme' ;
 
 test('renders Greet without crashing', () => {
   const div = document.createElement('div');
@@ -21,4 +22,33 @@ test('it changes state', () => {
  tree.props.onClick();
   tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+describe('Greet Component', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<Greet />);
+  });
+
+  it('has a greet div', () => {
+    expect(component.find("div.Greet").exists()).toBe(true);
+    expect(component.find("div.Greet").text()).toBe("This is a greet");
+  });
+});
+
+describe('Click on Component', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<Greet />);
+  });
+
+  it('changes message on click', () => {
+    expect(component.find("div.Greet").text()).toBe("This is a greet");
+
+    component.find("div.Greet").simulate("click");
+
+    expect(component.find("div.Greet").text()).toBe("I was clicked");
+  });
 });
