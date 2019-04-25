c0532f49f24bad08e1d16a8bd828b9bf2d3cebd0
pxai <pello.altadill@gmail.com>
2019-04-25 19:34:27 +0200

    Changed quotes

--- other/webpack-react-sample/webpack.config.clean.js
+++ other/webpack-react-sample/webpack.config.clean.js
@@ -3,46 +3,48 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPluglin = require("clean-webpack-plugin");
 
-const vendors = ["react","react-dom"];
+const vendors = ["react", "react-dom"];
 
 module.exports = {
-    entry: {
-        bundle: path.join(__dirname, 'src/index.js'),
-        vendor: vendors
-    },
-        output: {
-            path: path.resolve(__dirname, "build"),
-            filename: "[name].[chunkhash].js"
-    },
-    module: {
-        rules: [
-            {
-                use: "babel-loader",
-                test: /\.js$/,
-                exclude: /node_modules/
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "index.html",
-            filename: "index.html"
-        }),
-        new CleanWebpackPluglin({options: "build/*.*"})
-    ],
-    optimization: {
-        splitChunks: {
-          cacheGroups: {
-            commons: {
-              test: /[\\/]node_modules[\\/]/,
-              name: "vendor",
-              chunks: "initial",
-            },
-          },
+  entry: {
+    bundle: path.join(__dirname, 'src/index.js'),
+    vendor: vendors
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      filename: "index.html"
+    }),
+    new CleanWebpackPluglin({options: "build/*.*"})
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "initial",
         },
       },
-    stats: {
-        colors: true
     },
-    devtool: 'source-map'
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
 }
+
+
