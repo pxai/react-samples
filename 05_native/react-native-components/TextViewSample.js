800e565a1cebc9943b74fe9d57fd38e877413997
pxai <pello.altadill@gluckgames.com>
2019-06-13 00:19:10 +0200

    Almost there

--- 05_native/react-native-components/TextViewSample.js
+++ 05_native/react-native-components/TextViewSample.js
@@ -3,40 +3,40 @@ import { Font } from 'expo';
 import { StyleSheet, Text, View } from "react-native";
 
 export default class TextViewSample extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          fontLoaded: false,
-        };
-      
-    }
+	constructor (props) {
+		super (props);
+		this.state = {
+			fontLoaded: false,
+		};
 
-    async componentDidMount() {
-      await Font.loadAsync({'samplefont': require('./samplefont.ttf')});
-      this.setState({ fontLoaded: true });
-    }
+	}
 
-    message() {
-        return "This is important text";
-    }
+	async componentDidMount () {
+		await Font.loadAsync ({'samplefont': require ('./samplefont.ttf')});
+		this.setState ({fontLoaded: true});
+	}
 
-  render() {
-    return (
-      this.state.fontLoaded ? (
+	message () {
+		return "This is important text";
+	}
+
+	render () {
+		return (
+			this.state.fontLoaded ? (
         <View>
-          <Text style={{ fontFamily: 'samplefont', fontSize: 16 }}>Text components</Text>
+          <Text style={{fontFamily: 'samplefont', fontSize: 16}}>Text components</Text>
           <Text>Text can have nested text like this
-              <Text>{this.message()}</Text>
-              And it"s awesome.
+            <Text>{this.message ()}</Text>
+            And it"s awesome.
           </Text>
         </View>
-    ): null);
-  }
+			) : null);
+	}
 }
 
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 14
-  },
+const styles = StyleSheet.create ({
+	title: {
+		fontSize: 14
+	},
 });
 
